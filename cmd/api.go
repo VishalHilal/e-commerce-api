e5de634a5d268de7daccec1c9e0d14b42829a8e7
VishalHilal <vishalschool24@gmail.com>
2025-12-27 21:52:04 +0530

    api added

---
+++ cmd/api.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+	repo "github.com/VishalHilal/e-commerce-api/internal/adapters/postgresql/sqlc"
+	"github.com/VishalHilal/e-commerce-api/internal/orders"
+	"github.com/VishalHilal/e-commerce-api/internal/products"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID) 
+	r.Use(middleware.RealIP)    
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good"))
+	})
+
+	productService := products.NewService(repo.New(app.db))
+	productHandler := products.NewHandler(productService)
+	r.Get("/products", productHandler.ListProducts)
+
+	orderService := orders.NewService(repo.New(app.db), app.db)
+	ordersHandler := orders.NewHandler(orderService)
+	r.Post("/orders", ordersHandler.PlaceOrder)
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at addr %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
+
+type application struct {
+	config config
+	// logger
+	db *pgx.Conn
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
